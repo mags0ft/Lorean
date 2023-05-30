88b96241aedd1dbca16aa2018db4957153e645be
mags0ft <magnusschlinsog@gmail.com>
2023-05-30 16:55:02 +0200

    added backup manager

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -227,7 +227,6 @@ def recovery_pick():
 def examine_backup():
     g.custom_flashes = True
 
-    DISCOURAGE = "It is highly discouraged to continue as it might be extremely risky."
 
     to_recover = request.form["to-recover"]
     backup = request.form["backup"]
@@ -236,61 +235,7 @@ def examine_backup():
         abs_backup_path, backup
     )
 
-    try:
-        with open(os.path.join(
-            full_path, ".lorean_meta"
-        ), "r") as f:
-            meta = json.load(f)
-
-        flash(f"Meta information file seems to be intact.", "success")
-    except FileNotFoundError:
-        flash(f'''CRITICAL: no backup meta file has been found. This probably means that
-        the picked folder is corrupted or not even a valid backup made with Lorean. 
-        {DISCOURAGE}''', "danger")
-        meta = None
-    
-    except json.JSONDecodeError:
-        flash(f'''CRITICAL: the backup's meta information failed to load. This could mean your
-        backup is encrypted or corrupted. {DISCOURAGE}''', "danger")
-        meta = None
-
-    if meta != None:
-        if meta["lock"]:
-            flash('''CRITICAL: according to the backup meta file, the backup has not been completed.
-            Therefore, it might be missing many important files.''', "danger")
-        else:
-            flash("The backup seems to have been completed.", "success")
-
-        if meta["folder"] != to_recover:
-            flash(f'''WARNING: this backup seems to contain files intended for another directory.
-            Maybe you renamed your folder or picked a wrong backup. Please double-check everything
-            before you continue!''', "danger")
-        else:
-            flash("Folders of recovery and backup match.", "success")
-
-        total_files = sum(
-            len(_files) for _, _, _files in os.walk(
-                full_path
-            )
-        ) - 1 # for the meta file
-        if meta["files"] > total_files:
-            flash(
-                f'''WARNING: The backup is missing {meta['files'] - total_files} file(s) in it and
-                is therefore incomplete.''',
-                "danger"
-            )
-        elif meta["files"] < total_files:
-            flash(
-                f'''WARNING: The backup contains {total_files - meta['files']} file(s) more than
-                originally expected according to the meta information file. It seems like it has
-                been tampered with.''',
-                "danger"
-            )
-        else:
-            flash("Could validate that the backup is not missing files.", "success")
-
-    else:
-        flash("Skipped all security checks due to missing meta file.", "danger")
+    run_validation(full_path, flash, to_recover)
 
     return render_template(
         "recover/examine.html",
@@ -299,6 +244,69 @@ def examine_backup():
         to_recover = to_recover,
         backup_path = full_path
     )
+
+def run_validation(
+        full_path,
+        func,
+        to_recover = None
+    ):
+    DISCOURAGE = "It is highly discouraged to continue as it might be extremely risky."
+
+    try:
+        with open(os.path.join(
+            full_path, ".lorean_meta"
+        ), "r") as f:
+            meta = json.load(f)
+
+        func(f"Meta information file seems to be intact.", "success")
+    except FileNotFoundError:
+        func(f'''CRITICAL: no backup meta file has been found. This probably means that
+        the picked folder is corrupted or not even a valid backup made with Lorean. 
+        {DISCOURAGE}''', "danger")
+        meta = None
+    
+    except json.JSONDecodeError:
+        func(f'''CRITICAL: the backup's meta information failed to load. This could mean your
+        backup is encrypted or corrupted. {DISCOURAGE}''', "danger")
+        meta = None
+
+    if meta != None:
+        if meta["lock"]:
+            func('''CRITICAL: according to the backup meta file, the backup has not been completed.
+            Therefore, it might be missing many important files.''', "danger")
+        else:
+            func("The backup seems to have been completed.", "success")
+
+        if (meta["folder"] != to_recover) and (not to_recover == None):
+            func(f'''WARNING: this backup seems to contain files intended for another directory.
+            Maybe you renamed your folder or picked a wrong backup. Please double-check everything
+            before you continue!''', "danger")
+        else:
+            func("Folders of recovery and backup match.", "success")
+
+        total_files = sum(
+            len(_files) for _, _, _files in os.walk(
+                full_path
+            )
+        ) - 1 # for the meta file
+        if meta["files"] > total_files:
+            func(
+                f'''WARNING: The backup is missing {meta['files'] - total_files} file(s) in it and
+                is therefore incomplete.''',
+                "danger"
+            )
+        elif meta["files"] < total_files:
+            func(
+                f'''WARNING: The backup contains {total_files - meta['files']} file(s) more than
+                originally expected according to the meta information file. It seems like it has
+                been tampered with.''',
+                "danger"
+            )
+        else:
+            func("Could validate that the backup is not missing files.", "success")
+
+    else:
+        func("Skipped all security checks due to missing meta file.", "danger")
 
 @main.route("/backup/recover/run", methods = ["POST"])
 def run_recovery():
@@ -314,3 +322,48 @@ def run_recovery():
     })
 
     return redirect(url_for("main.monitor_backup"))
+
+@main.route("/manage", methods = ["GET", "POST"])
+def manage_backups():
+    if request.method.upper() == "GET":
+        return render_template(
+            "manage/begin.html",
+            locations = db["locations"]
+        )
+    
+    directory = request.form["dest"]
+
+    def dir_size_mb(dir_):
+        total_size = 0
+
+        for dir_path, _, filenames in os.walk(dir_):
+            for file in filenames:
+                file_path = os.path.join(dir_path, file)
+                if os.path.islink(file_path): return
+
+                total_size += os.path.getsize(file_path)
+
+        return round(total_size / (1024 ** 2), 2)
+    
+    backups = {}
+    for b in os.listdir(directory):
+        p = os.path.join(directory, b)
+        if not os.path.isdir(p): return
+
+        backups[b] = {
+            "size": 0,
+            "errors": []
+        }
+
+        run_validation(
+            p,
+            lambda err, status: backups[b]["errors"].append(err) if status != "success" else 0
+        )
+
+        backups[b]["size"] = dir_size_mb(p)
+    
+    return render_template(
+        "manage/view.html",
+        backups = backups,
+        path = directory
+    )
