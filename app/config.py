236b036655c1a05c4e1b7da9ac87f51f19e857aa
mags0ft <113589312+mags0ft@users.noreply.github.com>
2025-08-27 00:02:59 +0900

    add explanations to config file

--- app/config.py
+++ app/config.py
@@ -1,9 +1,13 @@
+"""
+General configuration file.
+"""
+
 DEBUG                   = False
 
 ENVFILE                 = ".env"
-
 LOGDIR                  = "logs"
 
-# WARNING! Setting these to "True" can make your logs get many megabytes big!
+# WARNING!
+# Setting any of these to "True" can make your logs get many megabytes big!
 LOG_SKIPS               = False
 LOG_COPY                = False
