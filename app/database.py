cc8dc287cd9849a769addcfd10d4ee029a7aba9f
mags0ft <113589312+mags0ft@users.noreply.github.com>
2025-08-27 00:06:45 +0900

    improve code quality of database.py

--- app/database.py
+++ app/database.py
@@ -1,15 +1,27 @@
+"""
+This module handles the creation and management of the database.
+"""
+
 from sqlitedict import SqliteDict
+
 
 STD_SETUP = {
     "excludes": [],
     "locations": []
 }
 
+
 def create_database():
+    """
+    Creates the boilerplate database schema.
+    """
+    
     db = SqliteDict("user_data.sqlite", autocommit = True)
 
     for key in STD_SETUP.keys():
-        if not key in db.keys():
-            db[key] = STD_SETUP[key]
+        if key in db.keys():
+            continue
+
+        db[key] = STD_SETUP[key]
 
     return db
