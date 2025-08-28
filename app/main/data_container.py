a1c8a130fc58b9f57d9d66cd3aeb01012f074367
mags0ft <113589312+mags0ft@users.noreply.github.com>
2025-08-29 00:30:19 +0900

    add docstrings to Progress class and constructor

--- app/main/data_container.py
+++ app/main/data_container.py
@@ -1,4 +1,13 @@
+"""
+Module for any data containing classes.
+TODO: consider migration to dataclasses.
+"""
+
 class Progress:
+    """
+    Class to keep track of the current job progress.
+    """
+    
     current_file = ""
 
     current_file_nr = 0
@@ -8,6 +17,10 @@ class Progress:
     description = "Copying files to target directory..."
 
     def __init__(self, current_file, current_file_nr, total_file_nr, skipped, description):
+        """
+        Initializes the progress class.
+        """
+        
         (
             self.current_file,
             self.current_file_nr,
