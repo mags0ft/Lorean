2678e65d533a697e618525309c3323b42227f606
mags0ft <113589312+mags0ft@users.noreply.github.com>
2025-09-06 23:26:40 +0200

    improve code quality of __init__.py

--- __init__.py
+++ __init__.py
@@ -1,19 +1,31 @@
+"""
+Main entry point for the Lorean application.
+"""
+
 import multiprocessing
+
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
 
-BANNER = '''
+BANNER = """
 Lorean is starting as packaged standalone.
-'''
+"""
 
 try:
     from .app import create_app
 except ImportError:
     from app import create_app
-    
-if __name__ == "__main__":
+
+def main():
+    """
+    Main function for Lorean to start the server.
+    """
+
     print(BANNER)    
 
     app = create_app(standalone = True)
     app.run()
+
+if __name__ == "__main__":
+    main()
